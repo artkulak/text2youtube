d0a879d64c732fd24e4c2a119ed921115bdb65c7
Movses <68658828+mirmozavr@users.noreply.github.com>
2023-11-13 00:50:42 +0500

    Switch to example.env.yaml if no env.yaml (#5)
    Config test tweak

--- src/tests/test_config.py
+++ src/tests/test_config.py
@@ -1,4 +1,3 @@
-from pathlib import Path
 from unittest.mock import patch
 
 from src.config import Config, get_config
@@ -14,14 +13,14 @@ conf_dict = {
 
 
 class TestConfigFunctions:
-    @patch("builtins.open", create=True)
     @patch(
         "src.config.safe_load",
         return_value=conf_dict,
     )
-    def test_get_config_with_custom_path(self, mock_safe_load, mock_open):
+    def test_get_config_with_custom_path(self, mock_safe_load):
         custom_path = "custom_path.yaml"
         config = get_config(custom_path)
+        mock_safe_load.assert_called_once()
         assert config == Config(
             OPENAI_API_KEY="your_key",
             OPENAI_PROMPTS_PATH="path",
@@ -30,24 +29,3 @@ class TestConfigFunctions:
             OUTPUT_DIR="output",
             YT_PROBA=80,
         )
-        mock_open.assert_called_once_with(custom_path, encoding="utf-8")
-        mock_safe_load.assert_called_once()
-
-    @patch("builtins.open", create=True)
-    @patch(
-        "src.config.safe_load",
-        return_value=conf_dict,
-    )
-    def test_get_config_with_default_path(self, mock_safe_load, mock_open):
-        default_path = Path(__file__).parent.parent.parent.joinpath("env.yaml")
-        config = get_config()
-        assert config == Config(
-            OPENAI_API_KEY="your_key",
-            OPENAI_PROMPTS_PATH="path",
-            SOURCE_DIR="source",
-            PROCESS_DIR="process",
-            OUTPUT_DIR="output",
-            YT_PROBA=80,
-        )
-        mock_open.assert_called_once_with(default_path, encoding="utf-8")
-        mock_safe_load.assert_called_once()
